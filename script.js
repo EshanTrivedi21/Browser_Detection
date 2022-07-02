731c495ae0fa2d5e1700b611139290cacb9736ab
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-07-02 17:52:15 +0530

    Final V1

--- script.js
+++ script.js
@@ -22,18 +22,23 @@ const osList =
 
 let browserChecker = () => {
     let userDetails = navigator.userAgent;
+
     for (let i in browserList) {
+
         if (userDetails.includes(browserList[i].value)) {
             browserDetailsRef.innerHTML = browserList[i].name || "Unknown Browser";
             break;
         }
+
     }
 
     for (let i in osList) {
+
         if (userDetails.includes(osList[i].value)) {
             osDetailsRef.innerHTML = osList[i].name;
             break;
         }
+        
     }
 }
 
